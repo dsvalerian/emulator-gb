d82d31b3a4f73e724ee3e97c9cb5adc09a7ac498
Dmitri Salov <dmitrisalov98@gmail.com>
2024-02-29 23:27:27 -0600

    Changing main method

--- src/main/kotlin/com/github/dsvalerian/emu/gb/GameBoy.kt
+++ src/main/kotlin/com/github/dsvalerian/emu/gb/GameBoy.kt
@@ -3,8 +3,10 @@ package com.github.dsvalerian.emu.gb
 import com.github.dsvalerian.emu.gb.cpu.Cpu
 
 class GameBoy {
-    init {
-        val bus = Bus()
-        val cpu = Cpu(bus)
+    private val bus: Bus = Bus()
+    private val cpu: Cpu = Cpu(bus)
+
+    fun start() {
+        // todo
     }
 }
--- src/main/kotlin/com/github/dsvalerian/emu/gb/Main.kt
+++ src/main/kotlin/com/github/dsvalerian/emu/gb/Main.kt
@@ -2,4 +2,5 @@ package com.github.dsvalerian.emu.gb
 
 fun main() {
     val gb = GameBoy()
+    gb.start()
 }
